3ac9546facec6eb0f3738baf37896a93702877db
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-11-01 16:25:44 -0600

    isThisSubnet needs testing

---
+++ problem1/isThisSubnet.py
@@ -0,0 +1,26 @@
+#IsThisSubnet
+import logging
+from hexConvert import hexConvert as hc
+from subnetRange import subnetRange as sr
+class isThisSubnet:
+    def __init__(self,inputIp,cidrSubnet):
+        self.inputIp = hc(inputIp).getIp()
+        logging.debug("CLASS isThisSubnet:__init__,inputIp="+str(self.inputIp))
+        self.cidrSubnetInfo = sr(cidrSubnet)
+        logging.debug("CLASS isThisSubnet:__init__,cidrSubnetInfo="+str(cidrSubnet))
+        self.blockNumber = self.cidrSubnetInfo.block
+        logging.debug("CLASS isThisSubnet:__init__,blockNumber="+str(self.blockNumber))
+        self.inputIpCidrSubnet = self.inputIp+"/"+str(self.blockNumber)
+        logging.debug("CLASS isThisSubnet:__init__,inputIpCidrSubnet="+str(self.inputIpCidrSubnet))
+        self.inputIpCidrSubnetInfo = sr(self.inputIpCidrSubnet)
+    def verify(self):
+        condition1 = (self.inputIpCidrSubnetInfo.networkIp == self.cidrSubnetInfo.networkIp)
+        logging.debug("CLASS isThisSubnet:verify,condition1(NetworkIp)="+str(condition1))
+        condition2 = (self.inputIpCidrSubnetInfo.broadcastIp == self.cidrSubnetInfo.broadcastIp)
+        logging.debug("CLASS isThisSubnet:verify,condition2(BroadcastIp)="+str(condition2))
+        if (condition1 and condition2):
+            logging.debug("CLASS isThisSubnet:verify,condition1 and condition2="+str(True))
+            return True
+        else:
+            logging.debug("CLASS isThisSubnet:verify,condition1 and condition2="+str(False))
+            return False
--- problem1/tests.py
+++ problem1/tests.py
@@ -1,9 +1,11 @@
 #testHExConvert
 from hexConvert import hexConvert
 from subnetRange import subnetRange
+from isThisSubnet import isThisSubnet
 import unittest
 import logging
 import sys
+"""
 class testHexConvert(unittest.TestCase):
     sample = '0x62D2ED4B'
     sampleBin = '1100010110100101110110101001011'
@@ -53,7 +55,18 @@ class testSubnetRange(unittest.TestCase):
         self.assertEqual(test_init.broadcastIp,test_init3.broadcastIp)
         test_init4 = subnetRange(self.sampleForCompare)
         self.assertNotEqual(test_init.networkIp,test_init4.networkIp)
-        self.assertNotEqual(test_init.broadcastIp,test_init4.broadcastIp)
+        self.assertNotEqual(test_init.broadcastIp,test_init4.broadcastIp)"""
+class test_isThisSubnet(unittest.TestCase):
+    sampleInput = '0x62D2ED4B'
+    sampleCidrSubnet = '98.210.237.192/26'
+    sampleInSubnet = '0x62D2EDC1'
+    result = False
+    resultInSubnet = True
+    def testInit(self):
+        #compute = isThisSubnet(self.sampleInput,self.sampleCidrSubnet)
+        #self.assertEqual(compute.verify(),self.result)
+        compute2 = isThisSubnet(self.sampleInSubnet,self.sampleCidrSubnet)
+        self.assertEqual(compute2.verify(),self.resultInSubnet)
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     unittest.main()
