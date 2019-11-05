261a78a93200d34eb9b04d4feba5bb1ca20e1c39
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-11-05 13:50:07 -0700

    added route latLongPairs that accepts lat & long pair values as /lat/lng

--- problem3/app.py
+++ problem3/app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def info():
+    return "Application Endpoint is :- http://127.0.0.1:5000/LatitudeValue/LongitudeValue"
+
+@app.route('/<lat>/<lng>')
+def latLongPairs(lat,lng):
+    return "Hello!\nLAT:{}\nLNG:{}".format(lat,lng)
 
 if __name__ == '__main__':
     app.run()
