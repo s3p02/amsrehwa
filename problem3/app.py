bfa6eebc34c130388ac0545bb3e9cd0b479c3481
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-11-05 15:54:34 -0700

    counts and cache clearing need to be tested

--- problem3/app.py
+++ problem3/app.py
@@ -3,6 +3,35 @@ from flask import request
 from flask import abort
 app = Flask(__name__)
 
+class lruCache:
+    hitCount = 0
+    missCount = 0
+    def __init__(self):
+        print("LRU CACHE")
+    
+    def _inc(self,cacheVal,o):
+        print("{} Prev: {}".format(cacheVal,o))
+        o += 1
+        print("{} Current: {}".format(cacheVal,o))
+
+    def inc(self,option):
+        optionDict = {
+            "hit": self._inc("hit",self.hitCount),
+            "miss": self._inc("miss",self.missCount)
+        }
+        optionDict[option]
+    
+    def clearHit(self):
+        print("clearHit Prev: {}".format(self.hitCount))
+        self.hitCount = 0
+        print("clearHit Current: {}".format(self.hitCount))
+    
+    def clearMiss(self):
+        print("clearHit Prev: {}".format(self.missCount))
+        self.missCount = 0
+        print("clearHit Current: {}".format(self.missCount))
+
+a = lruCache()
 
 @app.route('/')
 def info():
@@ -12,6 +41,33 @@ def info():
     <h2>-180 >= lat <=180 </h2>
     '''
     return _info
+
+
+@app.route('/clearHit')
+def clearHit():
+    a.clearHit()
+    curr = a.hitCount
+    clearHitMessage = '''<h1>Cur hitCount: {}</h1>'''.format(curr)
+    return clearHitMessage
+
+@app.route('/getHit')
+def getHit():
+    curr = a.hitCount
+    getHitMessage = '''<h1>Cur hitCount: {}</h1>'''.format(curr)
+    return getHitMessage
+
+@app.route('/clearMiss')
+def clearMiss():
+    a.clearMiss()
+    curr = a.missCount
+    clearMissMessage = '''<h1>Cur clearMiss: {}</h1>'''.format(curr)
+    return clearMissMessage
+
+@app.route('/getMiss')
+def getMiss():
+    curr = a.missCount
+    getMissMessage = '''<h1>Cur missCount: {}</h1>'''.format(curr)
+    return getMissMessage
 
 @app.route('/example-route')
 def example_route():
@@ -26,8 +82,10 @@ def example_route():
         if checkCoordinates(lat,lng):
             compute = '''<h1>The Latitude value is: {}</h1>
               <h1>The Longitude value is: {}</h1>'''.format(lat,lng)
+            a.inc("hit")
             return compute
         else:
+            a.inc("miss")
             abort(400)
     except ValueError:
         abort(400)
