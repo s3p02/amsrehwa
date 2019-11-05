80c053adf80d15d3122543455cf54f19558d0c35
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-11-05 14:13:33 -0700

    /example-route complete, manually tested

--- problem3/app.py
+++ problem3/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
 @app.route('/')
 def info():
-    return "Application Endpoint is :- http://127.0.0.1:5000/LatitudeValue/LongitudeValue"
+    return "Sample for Application Endpoint is :- http://127.0.0.1:5000/example-route?lat=90&lng=180"
 
-@app.route('/<lat>/<lng>')
-def latLongPairs(lat,lng):
-    return "Hello!\nLAT:{}\nLNG:{}".format(lat,lng)
+@app.route('/example-route')
+def example_route():
+    lat = request.args['lat']
+    lng = request.args['lng']
+    compute = '''<h1>The Latitude value is: {}</h1>
+              <h1>The Longitude value is: {}</h1>'''.format(lat,lng)
+    return compute
 
 if __name__ == '__main__':
     app.run()
