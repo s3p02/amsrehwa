02f0750c55959b90d9c82e2374392683261a07fb
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-11-05 23:48:47 -0700

    lruCache counters for hit & miss tested manually

--- problem3/app.py
+++ problem3/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import abort
+import logging
+import sys
 app = Flask(__name__)
 
 class lruCache:
@@ -8,28 +10,30 @@ class lruCache:
     missCount = 0
     def __init__(self):
         print("LRU CACHE")
-    
-    def _inc(self,cacheVal,o):
-        print("{} Prev: {}".format(cacheVal,o))
-        o += 1
-        print("{} Current: {}".format(cacheVal,o))
 
     def inc(self,option):
-        optionDict = {
-            "hit": self._inc("hit",self.hitCount),
-            "miss": self._inc("miss",self.missCount)
-        }
-        optionDict[option]
+        if option == "hit":
+            prev = self.hitCount
+            print("CLASS lruCache:inc:incHit,prev: {}".format(prev))
+            self.hitCount += 1
+            curr = self.hitCount
+            print("CLASS lruCache:inc:incHit,curr: {}".format(curr))
+        elif option == "miss":
+            prev = self.missCount
+            print("CLASS lruCache:inc:incMiss,prev: {}".format(prev))
+            self.missCount += 1
+            curr = self.missCount
+            print("CLASS lruCache:inc:incMiss,curr: {}".format(curr))
     
     def clearHit(self):
-        print("clearHit Prev: {}".format(self.hitCount))
+        print("CLASS lruCache:clearHit,Prev: {}".format(self.hitCount))
         self.hitCount = 0
-        print("clearHit Current: {}".format(self.hitCount))
+        print("CLASS lruCache:clearHit,Current: {}".format(self.hitCount))
     
     def clearMiss(self):
-        print("clearHit Prev: {}".format(self.missCount))
+        print("CLASS lruCache:clearHit,Prev: {}".format(self.missCount))
         self.missCount = 0
-        print("clearHit Current: {}".format(self.missCount))
+        print("CLASS lruCache:clearHit,Current: {}".format(self.missCount))
 
 a = lruCache()
 
@@ -90,4 +94,5 @@ def example_route():
     except ValueError:
         abort(400)
 if __name__ == '__main__':
+    #logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     app.run()
